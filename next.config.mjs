ac8bd6d46354f83185f3ff8914590635b37b49d4
Vincent550102 <5020559@gmail.com>
2024-08-06 23:11:16 +0800

    Fix Url issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  reactStrictMode: true,
   images: {
     domains: ["www.gravatar.com"],
     unoptimized: true,
   },
+  trailingSlash: true,
+  basePath: "/",
 };
 
 console.log("url:", "https://localhost:3000");
